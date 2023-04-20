d2facf578d1c64d3e8c20e9b9eea245a7f6098c4
Alevtina Mashtaler <alya.mashtalez@gmail.com>
2023-04-20 11:12:28 +0300

    set up main page

---
+++ leroymerlinwtf/RecommendedTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  RecommendedTableViewCell.swift
+//  leroymerlinwtf
+//
+//  Created by Admin on 19.04.2023.
+//
+
+import UIKit
+
+class RecommendedTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
