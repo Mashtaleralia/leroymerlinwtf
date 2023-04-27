b43515d514cd2ca0c87e70b4a343601f93afa184
Alevtina Mashtaler <alya.mashtalez@gmail.com>
2023-04-27 19:13:22 +0300

    some commit

---
+++ leroymerlinwtf/CatalogCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CatalogCollectionViewCell.swift
+//  leroymerlinwtf
+//
+//  Created by Admin on 20.04.2023.
+//
+
+import UIKit
+
+class CatalogCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ leroymerlinwtf/EmptyListViewController.swift
@@ -0,0 +1,8 @@
+//
+//  EmptyListViewController.swift
+//  leroymerlinwtf
+//
+//  Created by Admin on 20.04.2023.
+//
+
+import Foundation
